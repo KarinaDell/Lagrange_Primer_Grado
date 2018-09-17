af466714c5ae3f8552d66795bed5a8d6a7c9e149
KarinaDell <36238027+KarinaDell@users.noreply.github.com>
2018-09-17 08:33:45 -0500

    Add files via upload

---
+++ Lagrange/Lagrange/frmLagrange.Designer.cs
@@ -0,0 +1,187 @@
+﻿namespace Lagrange
+{
+    partial class frmLagrange
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtx = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtx0 = new System.Windows.Forms.TextBox();
+            this.txtfx0 = new System.Windows.Forms.TextBox();
+            this.txtx1 = new System.Windows.Forms.TextBox();
+            this.txtfx1 = new System.Windows.Forms.TextBox();
+            this.bttnCalculate = new System.Windows.Forms.Button();
+            this.txtRes = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtx
+            // 
+            this.txtx.Location = new System.Drawing.Point(50, 12);
+            this.txtx.Name = "txtx";
+            this.txtx.Size = new System.Drawing.Size(100, 20);
+            this.txtx.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(14, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "x";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(12, 39);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(21, 16);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "x0";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(12, 65);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(32, 16);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "f(x0)";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(12, 91);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(21, 16);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "x1";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(12, 116);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(32, 16);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "f(x1)";
+            // 
+            // txtx0
+            // 
+            this.txtx0.Location = new System.Drawing.Point(50, 38);
+            this.txtx0.Name = "txtx0";
+            this.txtx0.Size = new System.Drawing.Size(100, 20);
+            this.txtx0.TabIndex = 8;
+            // 
+            // txtfx0
+            // 
+            this.txtfx0.Location = new System.Drawing.Point(50, 64);
+            this.txtfx0.Name = "txtfx0";
+            this.txtfx0.Size = new System.Drawing.Size(100, 20);
+            this.txtfx0.TabIndex = 9;
+            // 
+            // txtx1
+            // 
+            this.txtx1.Location = new System.Drawing.Point(50, 90);
+            this.txtx1.Name = "txtx1";
+            this.txtx1.Size = new System.Drawing.Size(100, 20);
+            this.txtx1.TabIndex = 10;
+            // 
+            // txtfx1
+            // 
+            this.txtfx1.Location = new System.Drawing.Point(50, 116);
+            this.txtfx1.Name = "txtfx1";
+            this.txtfx1.Size = new System.Drawing.Size(100, 20);
+            this.txtfx1.TabIndex = 12;
+            // 
+            // bttnCalculate
+            // 
+            this.bttnCalculate.Location = new System.Drawing.Point(12, 142);
+            this.bttnCalculate.Name = "bttnCalculate";
+            this.bttnCalculate.Size = new System.Drawing.Size(138, 45);
+            this.bttnCalculate.TabIndex = 13;
+            this.bttnCalculate.Text = "Calcular";
+            this.bttnCalculate.UseVisualStyleBackColor = true;
+            this.bttnCalculate.Click += new System.EventHandler(this.bttnCalculate_Click);
+            // 
+            // txtRes
+            // 
+            this.txtRes.Location = new System.Drawing.Point(12, 193);
+            this.txtRes.Name = "txtRes";
+            this.txtRes.Size = new System.Drawing.Size(138, 20);
+            this.txtRes.TabIndex = 14;
+            // 
+            // frmLagrange
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(162, 225);
+            this.Controls.Add(this.txtRes);
+            this.Controls.Add(this.bttnCalculate);
+            this.Controls.Add(this.txtfx1);
+            this.Controls.Add(this.txtx1);
+            this.Controls.Add(this.txtfx0);
+            this.Controls.Add(this.txtx0);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtx);
+            this.Name = "frmLagrange";
+            this.Text = "Lagrange";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtx;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtx0;
+        private System.Windows.Forms.TextBox txtfx0;
+        private System.Windows.Forms.TextBox txtx1;
+        private System.Windows.Forms.TextBox txtfx1;
+        private System.Windows.Forms.Button bttnCalculate;
+        private System.Windows.Forms.TextBox txtRes;
+    }
+}
+
---
+++ Lagrange/Lagrange/frmLagrange.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lagrange
+{
+    public partial class frmLagrange : Form
+    {
+        public frmLagrange()
+        {
+            InitializeComponent();
+        }
+
+        private void bttnCalculate_Click(object sender, EventArgs e)
+        {
+            double x, x0, fx0, x1, fx1, res;
+            x = Convert.ToDouble(txtx.Text);
+            x0 = Convert.ToDouble(txtx0.Text);
+            fx0 = Convert.ToDouble(txtfx0.Text);
+            x1 = Convert.ToDouble(txtx1.Text);
+            fx1 = Convert.ToDouble(txtfx1.Text);
+
+            res = ((x - x1) / (x0 - x1)) * fx0 + ((x - x0) / (x1 - x0)) * fx1;
+
+            txtRes.Text = res.ToString();
+        }
+    }
+}
